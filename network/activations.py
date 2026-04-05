b258a69bc087bbe2e25556ed04be6ad6df0e13c9
Atharva Pardeshi <atharva.exe@gmail.com>
2026-04-05 15:45:12 +0530

    feat: define sigmoid, sigmoid derivative, and softmax functions

---
+++ network/activations.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+def sigmoid(z):
+    return 1.0 / (1.0 + np.exp(-z))
+
+
+def sigmoid_prime(z):
+    s = sigmoid(z)
+    return s*(1-s)
+
+
+def softmax(z):
+    # subtract max for numerical stability
+    shifted = z - np.max(z, axis=1, keepdims=True)
+    exp_z = np.exp(shifted)
+    return exp_z / np.sum(exp_z, axis=1, keepdims=True)
